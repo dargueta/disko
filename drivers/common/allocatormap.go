57a0aab4c3eea142cf401ae2e1ead535447bf6bc
Diego Argueta <diego@goodrx.com>
2021-06-22 18:56:39 -0700

    Expand allocator map stuff

--- drivers/common/allocatormap.go
+++ drivers/common/allocatormap.go
@@ -9,9 +9,11 @@ import (
 	"github.com/dargueta/disko"
 )
 
+type UnitID uint32
+
 type Allocator struct {
 	AllocationBitmap   bitmap.Bitmap
-	lastAllocatedIndex uint
+	lastAllocatedIndex UnitID
 	TotalUnits         uint
 }
 
@@ -22,13 +24,13 @@ func NewAllocator(totalUnits uint) Allocator {
 	}
 }
 
-// AllocateBlock allocates the first available block it finds and returns its
-// index. If no blocks are available, it returns an error.
-func (alloc *Allocator) AllocateBlock() (BlockID, error) {
+// AllocateSingle allocates the first available unit it finds and returns its
+// index. If no units are available, it returns an error.
+func (alloc *Allocator) AllocateSingle() (UnitID, error) {
 	for i := uint(0); i < alloc.TotalUnits; i++ {
 		if !alloc.AllocationBitmap.Get(int(i)) {
 			alloc.AllocationBitmap.Set(int(i), true)
-			return BlockID(i), nil
+			return UnitID(i), nil
 		}
 	}
 
@@ -37,29 +39,31 @@ func (alloc *Allocator) AllocateBlock() (BlockID, error) {
 
 // FreeBlock frees an allocated block. Trying to free a block that is already
 // allocated will return the errno code EALREADY.
-func (alloc *Allocator) FreeBlock(block BlockID) error {
-	if block >= BlockID(alloc.TotalUnits) {
+func (alloc *Allocator) FreeSingle(unit UnitID) error {
+	if unit >= UnitID(alloc.TotalUnits) {
 		msg := fmt.Sprintf(
-			"invalid block id: %d not in range [0, %d)",
-			block,
+			"invalid unit id: %d not in range [0, %d)",
+			unit,
 			alloc.TotalUnits)
 		return disko.NewDriverErrorWithMessage(disko.EINVAL, msg)
 	}
-	if !alloc.AllocationBitmap.Get(int(block)) {
-		msg := fmt.Sprintf("block %d is already free", block)
+	if !alloc.AllocationBitmap.Get(int(unit)) {
+		msg := fmt.Sprintf("block %d is already free", unit)
 		return disko.NewDriverErrorWithMessage(disko.EALREADY, msg)
 	}
 
-	alloc.AllocationBitmap.Set(int(block), false)
+	alloc.AllocationBitmap.Set(int(unit), false)
 	return nil
 }
 
-func (manager *Allocator) findRun(count uint, value bool) (BlockID, error) {
+// FindContiguousValues returns the index of the beginning of a block of units
+// of length `count` and the same value as `value`.
+func (alloc *Allocator) FindContiguousValues(value bool, count uint) (UnitID, error) {
 	runSize := uint(0)
-	runStart := BlockID(0)
+	runStart := UnitID(0)
 
-	for i := uint(0); i < manager.TotalUnits; i++ {
-		bit := manager.AllocationBitmap.Get(int(i))
+	for i := uint(0); i < alloc.TotalUnits; i++ {
+		bit := alloc.AllocationBitmap.Get(int(i))
 		if bit == !value {
 			// We hit the opposite value we were looking for, so this is the end
 			// of the run. Reset the size to 0 and try again.
@@ -70,30 +74,68 @@ func (manager *Allocator) findRun(count uint, value bool) (BlockID, error) {
 		runSize++
 		if runSize == 1 {
 			// If runSize is 1 then that means it was 0 when we entered the loop,
-			// so this is the first unallocated block in our latest attempt at
+			// so this is the first unallocated unit in our latest attempt at
 			// finding a run. This is the beginning of our run.
-			runStart = BlockID(i)
+			runStart = UnitID(i)
 		} else if runSize == count {
 			// We found the last block we need in the contiguous run.
 			return runStart, nil
 		}
 	}
 
-	// We ran off the end of the bitmap before we reached the necessary count
-	// of blocks.
-	return BlockID(0), disko.NewDriverError(disko.ENOSPC)
+	// We ran off the end of the bitmap before we reached the necessary count.
+	return UnitID(0), disko.NewDriverError(disko.ENOSPC)
 }
 
-// AllocateContiguousBlocks allocates a set of contiguous blocks in a first-fit
-// manner.
-func (manager *Allocator) AllocateContiguousBlocks(count uint) (BlockID, error) {
-	runStart, err := manager.findRun(count, false)
+func (alloc *Allocator) HasContiguousValuesAt(start UnitID, value bool, count uint) bool {
+	runSize := uint(0)
+
+	for i := start; i < UnitID(alloc.TotalUnits); i++ {
+		if runSize == count {
+			// We found the last block we need in the contiguous run.
+			return true
+		}
+
+		bit := alloc.AllocationBitmap.Get(int(i))
+		if bit == !value {
+			// We hit the opposite value we were looking for, so this is the end
+			// of the run.
+			return false
+		}
+		runSize++
+	}
+
+	// We ran off the end of the bitmap before we reached the necessary count.
+	return false
+}
+
+// AllocateContiguous allocates a set of contiguous units in a first-fit manner.
+func (alloc *Allocator) AllocateContiguous(count uint) (UnitID, error) {
+	runStart, err := alloc.FindContiguousValues(false, count)
 	if err != nil {
-		return BlockID(0), err
+		return UnitID(0), err
 	}
 
 	for i := uint(0); i < count; i++ {
-		manager.AllocationBitmap.Set(int(i+uint(runStart)), true)
+		alloc.AllocationBitmap.Set(int(i+uint(runStart)), true)
 	}
 	return runStart, nil
+}
+
+func (alloc *Allocator) FreeContiguous(start UnitID, count uint) error {
+	if !alloc.HasContiguousValuesAt(start, true, count) {
+		msg := fmt.Sprintf(
+			"tried to free already free blocks: there aren't %d allocated blocks starting at %d",
+			count,
+			start)
+		return disko.NewDriverErrorWithMessage(disko.EINVAL, msg)
+	}
+
+	for i := uint(0); i < count; i++ {
+		err := alloc.FreeSingle(start + UnitID(i))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
