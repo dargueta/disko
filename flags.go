f2449ce176e0676fdacc81daf6ce2829a43ac6f0
Diego Argueta <620513-dargueta@users.noreply.github.com>
2022-02-18 09:30:13 -0800

    Start documenting mount flags

--- flags.go
+++ flags.go
@@ -1,5 +1,8 @@
 package disko
 
+////////////////////////////////////////////////////////////////////////////////
+// File attribute flags
+
 const (
 	S_IXOTH = 1 << iota
 	S_IWOTH = 1 << iota
@@ -33,10 +36,33 @@ const S_IRWXG = S_IXGRP | S_IWGRP | S_IRGRP
 const S_IRWXU = S_IXUSR | S_IWUSR | S_IRUSR
 
 ////////////////////////////////////////////////////////////////////////////////
+// Mount flags
+// Descriptions taken from https://man7.org/linux/man-pages/man2/mount.2.html
 
+// MS_RDONLY means: mount the file system as read-only.
 const MS_RDONLY = 0x00000001
+
+// MS_NOSUID means: Do not honor set-user-ID and set-group-ID bits or file
+// capabilities when executing programs from this filesystem.
+//
+// This is irrelevant, but the flag is included for the sake of completeness
+// (also I copied and pasted this from my system's header file and don't want to
+// change anything.)
 const MS_NOSUID = 0x00000002
+
+// MS_NODEV means: Do not allow access to devices (special files) on this
+// filesystem.
+//
+// This is irrelevant, but the flag is included for the sake of completeness
+// (also I copied and pasted this from my system's header file and don't want to
+// change anything.)
 const MS_NODEV = 0x00000004
+
+// MS_NOEXEC means: Do not allow programs to be executed from this filesystem.
+//
+// This is irrelevant, but the flag is included for the sake of completeness
+// (also I copied and pasted this from my system's header file and don't want to
+// change anything.)
 const MS_NOEXEC = 0x00000008
 const MS_SYNCHRONOUS = 0x00000010
 const MS_REMOUNT = 0x00000020
@@ -67,3 +93,5 @@ const MS_BORN = 0x20000000
 const MS_ACTIVE = 0x40000000
 const MS_MGC_MSK = 0xffff0000
 const MS_MGC_VAL = 0xc0ed0000
+
+const DEFAULT_MOUNT_FLAGS = MS_NOSUID | MS_NODEV | MS_NOEXEC | MS_REMOUNT
