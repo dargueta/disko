641e2b79a498015492a3d726faa17589799d978a
Diego Argueta <dargueta@users.noreply.github.com>
2022-09-05 20:18:27 -0700

    Add sentinel values for invalid blocks

--- file_systems/common/types.go
+++ file_systems/common/types.go
@@ -2,8 +2,13 @@
 // across multiple file system implementations.
 package common
 
+import "math"
+
 type LogicalBlock uint
 type PhysicalBlock uint
+
+const InvalidLogicalBlock = LogicalBlock(math.MaxUint)
+const InvalidPhysicalBlock = PhysicalBlock(math.MaxUint)
 
 // Truncator is an interface for objects that support a Truncate() method. This
 // method must behave just like [os.File.Truncate].
