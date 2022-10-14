38183eedcde16069cdd84279a2d519a4022f7087
Diego Argueta <dargueta@users.noreply.github.com>
2022-10-14 16:01:45 -0700

    Test more stuff

--- file_systems/common/basicstream/basicstream_test.go
+++ file_systems/common/basicstream/basicstream_test.go
@@ -2,6 +2,7 @@ package basicstream_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/dargueta/disko"
@@ -68,5 +69,85 @@ func TestBasicStreamNew__LessThanOneBlock(t *testing.T) {
 	}
 	if !bytes.Equal(rawExpectedData[:readSize], streamData) {
 		t.Error("data read from stream is not equal to the expected raw data")
+	}
+}
+
+// Read various sizes at various offsets from the beginning of the stream. This
+// only tests Seek() with [io.SeekStart] as `whence`.
+func TestBasicStreamNew__SeekStart(t *testing.T) {
+	cache := diskotest.CreateDefaultCache(128, 16, false, nil, t)
+	stream, err := basicstream.New(cache.Size(), cache, disko.O_RDONLY)
+	if err != nil {
+		t.Fatalf("failed to create stream: %s", err.Error())
+	}
+
+	byteOffsets := []int64{
+		// Beginning of the stream
+		0,
+		// Exactly at the second block
+		128,
+		// Within the first block
+		90,
+		// Starting within the third block
+		300,
+	}
+	readSizes := []int{
+		// One byte
+		1,
+		// Part of a block
+		93,
+		// One full block
+		128,
+		// Multiple blocks
+		829,
+	}
+
+	rawUnderlyingBytes, _ := cache.Data()
+	for _, readSize := range readSizes {
+		buffer := make([]byte, readSize)
+
+		for _, offset := range byteOffsets {
+			where, err := stream.Seek(offset, io.SeekStart)
+			if err != nil {
+				t.Errorf(
+					"failed to seek to absolute offset %d: %s",
+					offset,
+					err.Error(),
+				)
+				continue
+			} else if where != offset {
+				t.Errorf(
+					"expected Seek() to return %d, got %d",
+					offset,
+					where,
+				)
+			}
+
+			expectedData := rawUnderlyingBytes[offset : offset+int64(readSize)]
+
+			n, err := stream.Read(buffer)
+			if err != nil {
+				t.Errorf(
+					"failed to read %d bytes from absolute offset %d: %s",
+					readSize,
+					offset,
+					err.Error(),
+				)
+			} else if n != readSize {
+				t.Errorf(
+					"wrong read size: expected %d bytes, got %d",
+					readSize,
+					n,
+				)
+			}
+
+			if !bytes.Equal(expectedData, buffer) {
+				t.Errorf(
+					"%d bytes read at offset %d don't match expected data",
+					n,
+					offset,
+				)
+			}
+		}
 	}
 }
