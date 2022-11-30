d53613a512e4ac323f15d0fb714442c67942d920
Diego Argueta <dargueta@users.noreply.github.com>
2022-11-29 16:34:09 -0800

    Collapse PredefinedDiskProfile into DiskGeometry

--- disks/disks.go
+++ disks/disks.go
@@ -8,21 +8,16 @@ import (
 )
 
 type DiskGeometry struct {
-	BitsPerAddressUnit    uint `csv:"bits_per_address_unit"`
-	AddressUnitsPerSector uint `csv:"address_units_per_sector"`
-	SectorsPerTrack       uint `csv:"sectors_per_track"`
-	TotalDataTracks       uint `csv:"total_data_tracks"`
-	TotalTracks           uint `csv:"total_tracks"`
-}
-
-type PredefinedDiskProfile struct {
-	Name               string `csv:"name"`
-	Slug               string `csv:"slug"`
-	FirstYearAvailable uint   `csv:"first_year_available"`
-	IsRemovable        uint   `csv:"is_removable"`
-	Notes              string `csv:"notes"`
-
-	DiskGeometry
+	Name                  string `csv:"name"`
+	slug                  string `csv:"slug"`
+	FirstYearAvailable    uint   `csv:"first_year_available"`
+	IsRemovable           uint   `csv:"is_removable"`
+	BitsPerAddressUnit    uint   `csv:"bits_per_address_unit"`
+	AddressUnitsPerSector uint   `csv:"address_units_per_sector"`
+	SectorsPerTrack       uint   `csv:"sectors_per_track"`
+	TotalDataTracks       uint   `csv:"total_data_tracks"`
+	TotalTracks           uint   `csv:"total_tracks"`
+	notes                 string `csv:"notes"`
 }
 
 type FormatterOptions struct {
@@ -32,22 +27,32 @@ type FormatterOptions struct {
 
 // go:embed disk-geometries.csv
 var diskGeometriesRawCSV string
-var diskGeometries map[string]PredefinedDiskProfile
+var diskGeometries map[string]DiskGeometry
+
+func GetPredefinedDiskGeometry(slug string) (DiskGeometry, error) {
+	geometry, ok := diskGeometries[slug]
+	if ok {
+		return geometry, nil
+	}
+
+	err := fmt.Errorf("no predefined disk geometry exists with slug %q", slug)
+	return DiskGeometry{}, err
+}
 
 func init() {
 	reader := strings.NewReader(diskGeometriesRawCSV)
 	err := gocsv.UnmarshalToCallback(
 		reader,
-		func(row PredefinedDiskProfile) error {
-			_, exists := diskGeometries[row.Slug]
+		func(row DiskGeometry) error {
+			_, exists := diskGeometries[row.slug]
 			if exists {
 				return fmt.Errorf(
 					"duplicate definition for disk %q found on row %d",
-					row.Slug,
+					row.slug,
 					len(diskGeometries)+1,
 				)
 			}
-			diskGeometries[row.Slug] = row
+			diskGeometries[row.slug] = row
 			return nil
 		},
 	)
