79f46fbdf18803cbbef6d7fb51d57c315a1015ed
Diego Argueta <dargueta@users.noreply.github.com>
2024-01-15 21:35:54 -0800

    (BROKEN) Add tests, tweak docs

--- utilities/compression/rle_grouper.go
+++ utilities/compression/rle_grouper.go
@@ -10,32 +10,35 @@ import (
 type ByteRun struct {
 	// Byte is the byte value for this run.
 	Byte byte
-	// RunLength gives the number of times the byte occurs in the run (not the
-	// number of times it's repeated).
+	// RunLength gives the number of times the byte occurs in the run.
 	//
 	// A valid run will always have this be 1 or greater. A value less than 1
 	// indicates either EOF was encountered, or an error occurred.
 	RunLength int
 }
 
-// InvalidRLERun is a sentinel value returned by [RLEGrouper.GetNextRun] when an
-// error occurred.
+// InvalidRLERun is a sentinel value returned by [RLEGrouper.GetNextRun] if an
+// error occurred, or EOF was encountered.
 var InvalidRLERun = ByteRun{0, 0}
 
-// RLEGrouper represents a
+// An RLEGrouper wraps an [io.Reader] and returns a [ByteRun] upon reads.
+//
+// This functions much like the `uniq` command line utility.
 type RLEGrouper struct {
 	rd *bufio.Reader
 }
 
-// NewRLEGrouper constructs an [RLEGrouper] that reads bytes from `rd` and
-// returns them serially in groups.
+// NewRLEGrouper constructs an [RLEGrouper].
 func NewRLEGrouper(rd io.Reader) RLEGrouper {
 	return RLEGrouper{rd: bufio.NewReader(rd)}
 }
 
 // GetNextRun returns a [ByteRun] for the next byte or run of byte values in the
-// stream. If an error occurred, the returned [ByteRun] struct will be equal to
-// [InvalidRLERun].
+// stream.
+//
+// The returned error behaves identically to [io.Reader.Read], namely that if
+// the returned run length is non-zero, the error will either be nil or [io.EOF].
+// If it's zero, the error is either [io.EOF] or another (non-nil) error.
 func (grouper RLEGrouper) GetNextRun() (ByteRun, error) {
 	firstByte, err := grouper.rd.ReadByte()
 	// Bail if any error occurred, including EOF.
@@ -43,20 +46,20 @@ func (grouper RLEGrouper) GetNextRun() (ByteRun, error) {
 		return InvalidRLERun, err
 	}
 
-	var runLength int
-	for runLength = 1; ; runLength++ {
+	for runLength := 1; ; runLength++ {
 		currentByte, err := grouper.rd.ReadByte()
+
 		if err != nil {
 			if errors.Is(err, io.EOF) {
-				break
+				return ByteRun{Byte: firstByte, RunLength: runLength}, io.EOF
 			}
 			return InvalidRLERun, err
 		}
+
 		if currentByte != firstByte {
 			// Hit a different byte, back up and return.
 			grouper.rd.UnreadByte()
-			break
+			return ByteRun{Byte: firstByte, RunLength: runLength}, nil
 		}
 	}
-	return ByteRun{Byte: firstByte, RunLength: runLength}, nil
 }
--- utilities/compression/rle_grouper_test.go
+++ utilities/compression/rle_grouper_test.go
@@ -2,12 +2,40 @@ package compression_test
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"io"
 	"testing"
 
 	c "github.com/dargueta/disko/utilities/compression"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+// A FailingReader is an [io.Reader] that returns a user-supplied error once the
+// given data (if any) has been exhausted.
+type FailingReader struct {
+	Data  io.Reader
+	Error error
+}
+
+// Read implements [io.Reader].
+func (fr FailingReader) Read(buf []byte) (int, error) {
+	n, err := fr.Data.Read(buf)
+	if err != nil && !errors.Is(err, io.EOF) {
+		panic(
+			fmt.Errorf(
+				"unexpected error getting %d bytes in FailingReader: %w",
+				len(buf),
+				err))
+	}
+
+	if n > 0 {
+		return n, err
+	}
+	// n == 0, input has been exhausted. We can now return the user's desired error.
+	return 0, fr.Error
+}
 
 type BasicTestCase struct {
 	Data           []byte
@@ -59,4 +87,42 @@ func TestRLEGrouper__Sequence(t *testing.T) {
 		}
 	}
 	assert.True(t, hitEOF, "never hit EOF sentinel")
+}
+
+func TestRLEGrouper__ErrorOnFirstRead(t *testing.T) {
+	expectedError := errors.New("this is the expected error")
+	reader := FailingReader{Data: &bytes.Buffer{}, Error: expectedError}
+
+	grouper := c.NewRLEGrouper(reader)
+	result, err := grouper.GetNextRun()
+
+	assert.ErrorIs(t, err, expectedError)
+	assert.Equal(t, c.InvalidRLERun, result)
+}
+
+func TestRLEGrouper__ErrorOnSubsequent(t *testing.T) {
+	expectedError := errors.New("this is the expected error")
+	reader := FailingReader{
+		Data:  bytes.NewBuffer([]byte{1, 1, 1, 2}),
+		Error: expectedError,
+	}
+
+	grouper := c.NewRLEGrouper(reader)
+
+	// First run
+	result, err := grouper.GetNextRun()
+	assert.Equal(t, byte(1), result.Byte, "byte is wrong for run 1")
+	assert.Equal(t, 3, result.RunLength, "run length is wrong for run 1")
+	require.NoError(t, err, "run 1 failed")
+
+	// Second run
+	result, err = grouper.GetNextRun()
+	assert.Equal(t, byte(2), result.Byte, "byte is wrong for run 2")
+	assert.Equal(t, 1, result.RunLength, "run length is wrong for run 2")
+	require.NoError(t, err, "run 2 failed")
+
+	// Third run should fail
+	result, err = grouper.GetNextRun()
+	assert.ErrorIs(t, err, expectedError)
+	assert.Equal(t, c.InvalidRLERun, result)
 }
