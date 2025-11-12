9354ad3b7ad2ea811a1b7fdbb855daa225c747f5
Diego Argueta <dargueta@users.noreply.github.com>
2025-11-11 20:58:04 -0800

    Add recursive RLE functions

--- utilities/compression/rle8.go
+++ utilities/compression/rle8.go
@@ -106,3 +106,121 @@ func DecompressRLE8(input io.Reader, output io.Writer) (int64, error) {
 		totalBytesWritten += int64(n)
 	}
 }
+
+// CompressRLERecursive is similar to CompressRLE8, but uses recursive run-length
+// encoding.
+//
+// It's identical to normal RLE8 for runs <= 256 bytes, but gets better compression
+// for runs over 258 bytes. It also has the advantage that long runs can themselves
+// be run-length encoded. For example, consider a run of 2048 nulls. This would
+// take 24 bytes with normal RLE8, or 11 bytes with recursive RLE:
+//
+//   - RLE8 : `00 00 FF 00 00 FF 00 00 FF 00 00 FF 00 00 FF 00 00 FF 00 00 FF 00 00 F7`
+//   - RRLE : `00 00 FF FF FF FF FF FF FF FF 06`
+//   - RRLE²: `00 00 00 FF FF 06 06`
+//
+// The contrast is even more stark for huge runs (say 128 KiB as part of a 7" floppy).
+// Using RRLE it would be 517 bytes: `00 00 FF{514 times} 00`, giving a compression
+// ratio of about 253:1. Because of the long run of FF, we can run RRLE on this again
+// and get to 9 bytes, a ratio of about 14,564:1. By contrast, RLE8 gives a compressed
+// size of 1530 bytes (86:1) and can't be reduced further.
+func CompressRLERecursive(input io.Reader, output io.Writer) (int64, error) {
+	grouper := NewRLEGrouperFromReader(input)
+
+	totalBytesWritten := int64(0)
+	for {
+		run, getRunErr := grouper.GetNextRun()
+		if getRunErr != nil && !errors.Is(getRunErr, io.EOF) {
+			// An error was encountered and it's *not* EOF.
+			return totalBytesWritten, getRunErr
+		}
+
+		if run.RunLength == 1 {
+			_, err := output.Write([]byte{run.Byte})
+			if err != nil {
+				return totalBytesWritten, err
+			}
+			totalBytesWritten++
+		} else if run.RunLength >= 2 {
+			_, err := output.Write([]byte{run.Byte, run.Byte})
+			if err != nil {
+				return totalBytesWritten, err
+			}
+			totalBytesWritten += 2
+
+			num255Repetitions := (run.RunLength - 2) / 255
+			remainder := (run.RunLength - 2) % 255
+
+			_, err = output.Write(bytes.Repeat([]byte{255}, num255Repetitions))
+			if err != nil {
+				return totalBytesWritten, err
+			}
+			totalBytesWritten += int64(num255Repetitions)
+
+			_, err = output.Write([]byte{byte(remainder)})
+			if err != nil {
+				return totalBytesWritten, err
+			}
+
+			totalBytesWritten++
+		}
+
+		// We bail at the beginning of the loop if an error occurred and it's
+		// *not* EOF, so if the error here is non-nil then that means it *must*
+		// be EOF. That means we finished without errors.
+		if getRunErr != nil {
+			return totalBytesWritten, nil
+		}
+	}
+}
+
+// DecompressRLERecursive is the decompression counterpart to CompressRLERecursive.
+func DecompressRLERecursive(input io.Reader, output io.Writer) (int64, error) {
+	grouper := NewRLEGrouperFromReader(input)
+
+	totalBytesWritten := int64(0)
+	for {
+		run, getRunErr := grouper.GetNextRun()
+		if getRunErr != nil && !errors.Is(getRunErr, io.EOF) {
+			// An error was encountered and it's *not* EOF.
+			return totalBytesWritten, getRunErr
+		}
+
+		if run.RunLength == 1 {
+			_, err := output.Write([]byte{run.Byte})
+			if err != nil {
+				return totalBytesWritten, err
+			}
+			totalBytesWritten++
+		} else if run.RunLength >= 2 {
+			_, err := output.Write([]byte{run.Byte, run.Byte})
+			if err != nil {
+				return totalBytesWritten, err
+			}
+			totalBytesWritten += 2
+
+			num255Repetitions := (run.RunLength - 2) / 255
+			remainder := (run.RunLength - 2) % 255
+
+			_, err = output.Write(bytes.Repeat([]byte{255}, num255Repetitions))
+			if err != nil {
+				return totalBytesWritten, err
+			}
+			totalBytesWritten += int64(num255Repetitions)
+
+			_, err = output.Write([]byte{byte(remainder)})
+			if err != nil {
+				return totalBytesWritten, err
+			}
+
+			totalBytesWritten++
+		}
+
+		// We bail at the beginning of the loop if an error occurred and it's
+		// *not* EOF, so if the error here is non-nil then that means it *must*
+		// be EOF. That means we finished without errors.
+		if getRunErr != nil {
+			return totalBytesWritten, nil
+		}
+	}
+}
